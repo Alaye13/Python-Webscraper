8d01130dd62114e2575d31afa38dc00964a4d1b2
Alaye13 <ifennaekwenem@gmail.com>
2024-02-23 14:48:13 -0500

    CSV Implementations

--- Scraper.py
+++ Scraper.py
@@ -3,77 +3,187 @@ import requests
 import csv
 
 # URL of the webpage containing the table
-url = "http://ufcstats.com/fight-details/a74a8c1e0a49070d"
+# URL of the webpage containing the table
+urls = [
+    "http://ufcstats.com/fight-details/a74a8c1e0a49070d",
+    "http://ufcstats.com/fight-details/ef13ed466e0b42d8",
+]
+
+with open("fight_data.csv", "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+
+        # Write the header row
+        header_row = [
+            "Name",
+            "Knockdown",
+            "Significant_Strikes_Landed",
+            "Significant_Strikes_Attempted",
+            "Significant_Strike_%",
+            "Total_Strikes_Landed",
+            "Total_Strikes_Attempted",
+            "Takedowns_Landed",
+            "Takedowns_Attempted",
+            "Takedowns_Percent",
+            "Submission_Attempts",
+            "REV",
+            "Control_Time",
+            "Head_Landed",
+            "Head_Attempted",
+            "Body_Landed",
+            "Body_Attempted",
+            "Leg_Landed",
+            "Leg_Attempted",
+            "Distance_Landed",
+            "Distance_Attempted",
+            "Clinch_Landed",
+            "Clinch_Attempted",
+            "Ground_Landed",
+            "Ground_Attempted",
+            "Winner",
+        ]
+        writer.writerow(header_row)
 
 # Send a GET request to the URL
-response = requests.get(url)
+        for url in urls:
+            response = requests.get(url)
 
-# Parse the HTML content
-soup = BeautifulSoup(response.content, "html.parser")
 
-# Find the table body element
-table_body = soup.find("tbody", class_="b-fight-details__table-body")
+            # Parse the HTML content
+            soup = BeautifulSoup(response.content, "html.parser")
 
-# Find all table rows within the table body
-table_rows = table_body.find_all("tr", class_="b-fight-details__table-row")
+            # Find the table body element & for the Significant Strikes table
+            table_body = soup.find("tbody", class_="b-fight-details__table-body")
+            page_data = soup.find_all("tr")
+            sig_table_body = soup.find("tbody", class_="b-fight-details__table-body")
 
-#print ("The number of table rows," , table_rows)
+            # Find all table rows within the table body
+            table_rows = table_body.find_all("tr", class_="b-fight-details__table-row")
 
-# Open a CSV file in write mode
-with open("fight_data.csv", "w", newline="") as csv_file:
-    writer = csv.writer(csv_file)
+            # Find for the Significant Strikes Table
+            sig_table_row = sig_table_body.find_all("tr", class_="b-fight-details__table-row")
+            # Significant Strikes
 
-    # Write the header row
-    header_row = [
-        "Name",
-        "Total Strikes Landed",       
-        "Total Strikes Attempted",     
-        "Significant Strikes Landed",
-        "Significant Strikes Attempted",
-        "Takedowns",
-        "Submission Attempts",
-        "Fight Time"
-    ]
-    writer.writerow(header_row)
+            head_sig_forfighter1 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(4) > p:nth-child(1)"
+            ).get_text(strip=True)
+            head_sig_forfighter2 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(4) > p:nth-child(2)"
+            ).get_text(strip=True)
+            body_sig_forfighter1 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(5) > p:nth-child(1)"
+            ).get_text(strip=True)
+            body_sig_forfighter2 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(5) > p:nth-child(2)"
+            ).get_text(strip=True)
+            leg_sig_forfighter1 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(6) > p:nth-child(1)"
+            ).get_text(strip=True)
+            leg_sig_forfighter2 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(6) > p:nth-child(2)"
+            ).get_text(strip=True)
+            distance_sig_forfighter1 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(7) > p:nth-child(1)"
+            ).get_text(strip=True)
+            distance_sig_forfighter2 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(7) > p:nth-child(2)"
+            ).get_text(strip=True)
+            clinch_sig_forfighter1 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(8) > p:nth-child(1)"
+            ).get_text(strip=True)
+            clinch_sig_forfighter2 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(8) > p:nth-child(2)"
+            ).get_text(strip=True)
+            ground_sig_forfighter1 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(9) > p:nth-child(1)"
+            ).get_text(strip=True)
+            ground_sig_forfighter2 = soup.select_one(
+                "body > section > div > div > table > tbody > tr > td:nth-child(9) > p:nth-child(2)"
+            ).get_text(strip=True)
+            winforfighter1 = soup.select_one(
+                "body > section > div > div > div.b-fight-details__persons.clearfix > div:nth-child(1) > i"
+            ).get_text(strip=True)
+            winforfighter2 = soup.select_one(
+                "body > section > div > div > div.b-fight-details__persons.clearfix > div:nth-child(2) > i"
+            ).get_text(strip=True)
 
-    # Loop through each table row to extract data
-    for row in table_rows:
-        # Extract fighter names
-        fighters = row.find_all("a", class_="b-link_style_black")
-        print(fighters)
-        fighter1 = fighters[0].get_text(strip=True)
-        fighter2 = fighters[1].get_text(strip=True)
 
-        # Extract all statistics for both fighters
-        stats = row.find_all("p", class_="b-fight-details__table-text")
-        stats = [stat.get_text(strip=True) for stat in stats]
-        print(stats)
+            # Open a CSV file in write mode
+            
+                # Loop through each table row to extract data
+            for row, sig_row in zip(
+                table_body.find_all("tr", class_="b-fight-details__table-row"),
+                sig_table_body.find_all("tr", class_="b-fight-details__table-row"),
+            ):
+                # for row, sig_row in zip(table_rows, sig_table_rows):
 
-        # Combine the extracted data
-        fighter1data = [
-            fighter1,
-            stats[2],             # Knockdown
-            stats[4].split()[0],  # Fighter 1 Significant Strikes Landed
-            stats[4].split()[2],  # Fighter 1 Significant Strikes Attempted
-            stats[6],             # Significant Strikes %
-            stats[8].split()[0],  
-            stats[8].split()[2],           # total strikes landed
-            stats[10].split()[0],  # Fighter 1 Takedowns
-            stats[10].split()[2]   #take down atttempted
-        ]
+                # Extract all statistics for both fighters
+                stats = row.find_all("p", class_="b-fight-details__table-text")
+                stats = [stat.get_text(strip=True) for stat in stats]
+                print(stats)
+                sig_stats = [
+                    data.get_text(strip=True)
+                    for data in sig_row.find_all("p", class_="b-fight-details__table-text")
+                ]
+                # sig_stats = [stat.get_text(strip=True) for stat in sig_stats]
+                print(sig_stats)
+                # Combine the extracted data
+                fighter1data = [
+                    stats[0],  # Name
+                    stats[2],  # Knockdown
+                    stats[4].split()[0],  # Fighter 1 Significant Strikes Landed
+                    stats[4].split()[2],  # Fighter 1 Significant Strikes Attempted
+                    stats[6],  # Significant Strikes %
+                    stats[8].split()[0],  # Total Strikes Landed
+                    stats[8].split()[2],  # total strikes Attempted
+                    stats[10].split()[0],  # Fighter 1 Takedowns
+                    stats[10].split()[2],  # take down attempted
+                    stats[12],  # TD %
+                    stats[14],  # Submission Attempts
+                    stats[16],  # REV
+                    stats[18],  # CTRL TIME
+                    head_sig_forfighter1.split()[0],
+                    head_sig_forfighter1.split()[2],
+                    body_sig_forfighter1.split()[0],
+                    body_sig_forfighter1.split()[2],
+                    leg_sig_forfighter1.split()[0],
+                    leg_sig_forfighter1.split()[2],
+                    distance_sig_forfighter1.split()[0],
+                    distance_sig_forfighter1.split()[2],
+                    clinch_sig_forfighter1.split()[0],
+                    clinch_sig_forfighter1.split()[2],
+                    ground_sig_forfighter1.split()[0],
+                    ground_sig_forfighter1.split()[2],
+                    winforfighter1,
+                ]
 
-        fighter2data = [
-                    fighter2,
-                    stats[3],             # Knockdown
+                fighter2data = [
+                    stats[1],  # Name
+                    stats[3],  # Knockdown
                     stats[5].split()[0],  # Fighter 1 Significant Strikes Landed
                     stats[5].split()[2],  # Fighter 1 Significant Strikes Attempted
-                    stats[7],             # Significant Strikes %
-                    stats[9].split()[0],  
-                    stats[9].split()[2],           # total strikes landed
+                    stats[7],  # Significant Strikes %
+                    stats[9].split()[0],  # Total Strikes Landed
+                    stats[9].split()[2],  # total strikes Attempted
                     stats[11].split()[0],  # Fighter 1 Takedowns
-                    stats[11].split()[2]   #take down atttempted
+                    stats[11].split()[2],  # take down attempted
+                    stats[13],  # TD %
+                    stats[15],  # Submission Attempts
+                    stats[17],  # REV
+                    stats[19],  # CTRL TIME
+                    head_sig_forfighter2.split()[0],
+                    head_sig_forfighter2.split()[2],
+                    body_sig_forfighter2.split()[0],
+                    body_sig_forfighter2.split()[2],
+                    leg_sig_forfighter2.split()[0],
+                    leg_sig_forfighter2.split()[2],
+                    distance_sig_forfighter2.split()[0],
+                    distance_sig_forfighter2.split()[2],
+                    clinch_sig_forfighter2.split()[0],
+                    clinch_sig_forfighter2.split()[2],
+                    ground_sig_forfighter2.split()[0],
+                    ground_sig_forfighter2.split()[2],
+                    winforfighter2,
                 ]
-        # Write the combined data to the CSV file
-        writer.writerow(fighter1data)
-        writer.writerow(fighter2data)
-
+                # Write the combined data to the CSV file
+                writer.writerow(fighter1data)
+                writer.writerow(fighter2data)
